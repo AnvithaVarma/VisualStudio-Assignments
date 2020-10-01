01b463e7b41d3c722bcf7ea100de88646c516399
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-01 21:22:25 +0530

    Add files via upload

---
+++ DigitSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitsSum
+{
+    class DigitSum
+    {
+        static void Main(string[] args)
+        {
+            int sum=0; 
+            Console.WriteLine("Enter the Number whose digits had to be added: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            while (num > 0 || sum>9)
+            {
+                
+                if (num==0)
+                {
+                    num = sum;
+                    sum = 0;
+                }
+             sum += num % 10;
+             num/= 10;
+            }
+            Console.WriteLine($"Sum of the digits are: {sum}");
+            Console.ReadLine();
+
+        }
+    }
+}
