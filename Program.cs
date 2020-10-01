ce7b3e55051242a1cdba88af71e76d1fa3d324cd
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-01 10:15:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter the number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine("{0}*{1}={2}", num, i, num * i);
+
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+
+}
