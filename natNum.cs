99b250052828a77dad2fac04d291f1f592e92525
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-01 21:27:01 +0530

    Add files via upload

---
+++ natNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("The first ten natural numbers are : ");
+            for (i = 1; i <= 10; i++)
+            {
+               Console.WriteLine($"{+i}");
+                Console.WriteLine("\n");
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
