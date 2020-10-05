6e45772b0255ce5f5066a474511ea8b25cdf4f9a
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-05 15:41:38 +0530

    Add files via upload

---
+++ dictionaryCities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictCities
+{
+    class dictionaryCities
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, String> dict = new Dictionary<int, string>();
+            dict.Add(1, "Bangalore");
+            dict.Add(2, "Mumbai");
+            dict.Add(3, "Pune");
+            dict.Add(4, "Hyderabad");
+            dict.Add(5, "Chennai");
+            dict.Add(6, "Delhi");
+            Console.WriteLine("The Elements in the Dictionary are :" );
+            //foreach (KeyValuePair<int, String> x in dict)
+            //{
+            //    Console.WriteLine( + x.Key + "   " + x.Value);
+            //}
+
+            dict[7] = "Kolkata";
+            dict[8] = "Ahmedabad";
+            dict[9] = "Noida";
+            dict[10] = "Tiruvunantapuram";
+            foreach (KeyValuePair<int, String> x in dict)
+            {
+                Console.WriteLine(+x.Key + "   " + x.Value);
+            }
+
+            dict.Remove(1);
+           // dict.Remove(2);
+           // dict.Remove(3);
+           // dict.Remove(4);
+           // dict.Remove(5);
+           // dict.Remove(6);
+            Console.WriteLine(" The cities remained after removing are : ");
+            foreach (KeyValuePair<int, String> x in dict)
+            {
+                Console.WriteLine( +x.Key + "   " + x.Value);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stackExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class stackExample
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> MyStack = new Stack<int>(); //created an object of stack
+            MyStack.Push(2); // push method takes one paramater
+            MyStack.Push(4);
+            MyStack.Push(6);
+            MyStack.Push(8);
+            MyStack.Push(10);
+            MyStack.Push(12);
+            MyStack.Push(14);
+            MyStack.Push(16);
+            MyStack.Push(18);
+            MyStack.Push(20);
+            Console.WriteLine("The elements of stack are: ");
+            foreach (int stack in MyStack)
+            {
+                Console.WriteLine( + stack);
+            }
+
+            MyStack.Pop();
+            MyStack.Pop();
+          //  MyStack.Pop();
+          //  MyStack.Pop();
+           // MyStack.Pop();
+           // MyStack.Pop();
+            Console.WriteLine("The elements of stack remained after pop of elements are: ");
+            foreach (int stack in MyStack)
+            {
+                Console.WriteLine(+stack);
+            }
+
+            Console.WriteLine("The peek element of stack is :" + MyStack.Peek());
+            
+        }
+    }
+}
