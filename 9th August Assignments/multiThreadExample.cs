072b914991bc0d76896315fe222f07ee748a0730
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-09 15:54:07 +0530

    Add files via upload

---
+++ 9th August Assignments/multiThreadExample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace MultiThreadEg
+{
+    class multiThreadExample
+    {
+        public string Thread1()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("Thread1 started at position : " + i);
+            }
+            return "Thread1 ended";
+
+           
+        }
+        public string Thread2()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("Thread2 started at position : " + i);
+            }
+            return "Thread2 ended";
+        }
+        public string Thread3()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                if (i == 5)
+                {
+                    Console.WriteLine("Thread3 sleeping for 5seconds");
+                    Thread.Sleep(5000);
+                }
+                Console.WriteLine("Thread3 started at position : " + i);
+            }
+            return "Thread3 ended";
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            multiThreadExample obj1 = new multiThreadExample();
+            string result = obj1.Thread1();
+            Console.WriteLine(result);
+            multiThreadExample obj2 = new multiThreadExample();
+            string result1 = obj2.Thread2();
+            Console.WriteLine(result1);
+            multiThreadExample obj3 = new multiThreadExample();
+            string result2 = obj3.Thread3();
+            Console.WriteLine(result2);
+
+        }
+    }
+}
---
+++ 9th August Assignments/singleThreadEg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleThreadExample
+{
+    class singleThreadEg
+    {
+        public string Thread()
+            
+        {
+             return "Thread1 has started!";           
+        }
+        
+        public string Thread2()
+        {
+            return "Thread2 has started!";
+        }
+
+        static void Main(string[] args)
+        {
+            singleThreadEg obj = new singleThreadEg();
+            string result = obj.Thread();
+              Console.WriteLine(result);
+            
+
+            singleThreadEg obj2 = new singleThreadEg();
+            string result2 = obj2.Thread2();
+            Console.WriteLine(result2);
+
+
+        }
+    }
+}
