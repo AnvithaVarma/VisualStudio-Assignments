0e290bbcbd9652b9aa0f99a616dd9d9a514e6a90
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-12 05:45:08 +0530

    Add files via upload

---
+++ WeekOverVies/ListsExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLists
+{
+    class ListsExample
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> colors = new List<string>();
+            colors.Add("Red");
+            colors.Add("Blue");
+            colors.Add("Green");
+            colors.Add("Violet");
+            colors.Add("Pink");
+            colors.Add("White");
+            colors.Count();
+            colors.Insert(1, "Orange");
+            Console.WriteLine("List is Created!");
+            
+            
+            foreach (string color in colors)
+            {
+                Console.WriteLine($"Elements in List are : {color}");
+            }
+            Console.WriteLine($"Number of Elements present in List are : " +colors.Count( ));
+
+            colors.Sort();
+            Console.WriteLine("List after being sorted is - ");
+            foreach (string color in colors)
+            {
+                Console.WriteLine($"Elements in List are : {color}");
+            }
+
+            if (colors.Contains("Blue"))
+            {
+                Console.WriteLine("Yes! Blue color exists in the list");
+            }
+            int index = colors.BinarySearch("Pink");
+            //Console.WriteLine($"Index of mentioned color is : " +colors.BinarySearch());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WeekOverVies/constructorInThread.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConstructorsInThread
+{
+   // public delegate string Test(string obj);
+    class constructorInThread
+    {
+        static void Test(object max)
+        {
+            int num = Convert.ToInt32(max);
+            for (int i = 1; i < num; i++)
+            {
+                Console.WriteLine("Thread1 has started!");
+                Console.WriteLine("Test" + i);
+
+            }
+            Console.WriteLine("Thread1 has Ended!");
+            // return "Test has ended";
+        }
+            static void Test1(object min)
+            {
+            int nums = Convert.ToInt32(min);
+            for (int i = 1; i < nums; i++)
+                {
+                    Console.WriteLine("Thread2 has started!");
+                    Console.WriteLine("Test1" + i);
+
+                }
+                Console.WriteLine("Thread2 has Ended!");
+            }
+        
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Thread started!");
+            //Thread t = new Thread(new ThreadStart(Test));
+            //Thread t1 = new Thread(new ThreadStart(Test1));//instance of thread
+            //ThreadStart obj = () => Test();   //using lambda expression!
+            //ThreadStart obj1 = () => Test1();// using lambda expression
+            
+            ParameterizedThreadStart obj = new ParameterizedThreadStart(Test); 
+            
+            ParameterizedThreadStart obj1 = new ParameterizedThreadStart(Test1);
+            Thread t = new Thread(obj);
+            Thread t1 = new Thread(obj1);
+            t.Start(5);
+            t1.Start(6);
+            //Test();
+            //Test1();
+            Console.WriteLine("Main Thread Ended!");
+            Console.ReadLine();
+        }
+    }
+}
