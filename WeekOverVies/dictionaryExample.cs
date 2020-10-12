015b3eb07104d19284123aabd946971fd03ed37c
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-12 05:48:50 +0530

    Add files via upload

---
+++ WeekOverVies/dictionaryExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+           
+            Dictionary<String, String> dict = new Dictionary<string, string>();
+            dict.Add("Category  ",  "Men Fashion");
+            dict.Add("List ",  "Women Fashion");
+            dict.Add("Accessories",  "Bangles");
+            dict.Add("Kids", "Jeans");
+
+            foreach(KeyValuePair<String,String> x in dict)
+            {
+                Console.WriteLine("The Elements in dictionary are : " + x.Key +"  "  + x.Value);
+            }
+
+            Console.WriteLine("Number of elements present are : " +dict.Count());
+            dict["FastTrack"] = "Watches";
+            foreach (KeyValuePair<String, String> x in dict)
+            {
+                Console.WriteLine("The Elements in dictionary are : " + x.Key +"  " + x.Value);
+            }
+
+           // Console.WriteLine("elements left after being deleted are : " + dict.Remove("List"));
+            foreach (KeyValuePair<String, String> x in dict)
+            {
+                Console.WriteLine("elements left after being deleted are : " + dict.Remove("List"));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
