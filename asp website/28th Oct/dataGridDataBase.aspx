aef074bc49891bb71b6794271d8a974aaca36776
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-10-28 13:29:15 +0530

    Add files via upload

---
+++ asp website/28th Oct/dataGridDataBase.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dataGridDataBase.aspx.cs" Inherits="October_28th.dataGridDataBase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DataGrid ID="DataGrid1" runat="server" OnSelectedIndexChanged="DataGrid1_SelectedIndexChanged" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ asp website/28th Oct/dataGridDataBase.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace October_28th
+{
+    public partial class dataGridDataBase : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection connect = new SqlConnection("Server = GB-PG00N58V; DataBase = Dxc Training; integrated security = true"))
+            {
+               // SqlDataAdapter sda = new SqlDataAdapter("select * from student", connect);
+               // SqlDataAdapter sda = new SqlDataAdapter("select * from Students", connect);
+               // SqlDataAdapter sda = new SqlDataAdapter("select * from Employee1", connect);
+                //SqlDataAdapter sda = new SqlDataAdapter("select * from IndianCricketTeam", connect);
+               // SqlDataAdapter sda = new SqlDataAdapter("select * from IndianRailwaySystem", connect);
+                SqlDataAdapter sda = new SqlDataAdapter("select * from BankTable", connect);
+
+                DataSet ds = new DataSet();
+
+                sda.Fill(ds);   
+
+                DataGrid1.DataSource = ds;
+                DataGrid1.DataBind();
+            }
+
+        }
+
+        protected void DataGrid1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
