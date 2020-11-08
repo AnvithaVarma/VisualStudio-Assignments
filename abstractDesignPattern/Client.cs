e3303d1341be2cf3f03ff3fe36ff67adcddc8af2
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-11-08 22:09:23 +0530

    Add files via upload

---
+++ abstractDesignPattern/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abstractFactoryExample.abstractProducts;
+
+namespace abstractFactoryExample
+{
+    class Client
+    {
+        private readonly Shirt _shirt;
+        private readonly Trousers _trousers;
+
+        public Client(clothesFactory factory)
+        {
+            _shirt = factory.CreateShirt();
+            _trousers = factory.CreateTrousers();
+        }
+
+        public string DescribeYourClothes()
+        {
+            return ($"Today i dressed in: {_shirt.GetType().Name} and {_trousers.GetType().Name}");
+        }
+
+    }
+}
---
+++ abstractDesignPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using abstractFactoryExample.factories;
+
+namespace abstractFactoryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client Anvitha = new Client(new casualClothes());
+            Console.WriteLine($"Anvitha: {Anvitha.DescribeYourClothes()}");
+
+            Client Sravya = new Client(new elegantClothes());
+            Console.WriteLine($"Sravya: {Sravya.DescribeYourClothes()}");
+
+            Console.ReadKey();
+        }
+    }
+}
