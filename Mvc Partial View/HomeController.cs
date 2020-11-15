6222c192d861adb9e322f365a3841487f8ca3d62
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-11-15 20:35:52 +0530

    Add files via upload

---
+++ Mvc Partial View/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace partialViewMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Login()
+        {
+            return PartialView();
+        }
+    }
+}
