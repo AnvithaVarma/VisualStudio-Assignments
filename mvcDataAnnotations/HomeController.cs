5d8b73125c5c7838df1c2e1534c75d983f079efb
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-11-15 20:07:26 +0530

    Add files via upload

---
+++ mvcDataAnnotations/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dataAnnotationsMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Anvi";
+            ViewBag.Check =1;
+            ViewBag.As = 123454;
+            ViewBag.LastName = "Varma";
+            return View();
+        }
+    }
+}
---
+++ mvcDataAnnotations/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dataAnnotationsMvc.Models
+{
+    public class Student
+    {
+        [Key]
+        public int stdID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollDate { get; set; }
+    }
+
+
+    public class Course
+    {
+        public int CourseID { get; set; }
+        [ConcurrencyCheck]
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        [Timestamp,DataType("timestamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+    public class Course1
+    {
+        public int CourseID { get; set; }
+        [ConcurrencyCheck]
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+
+    }
+}
