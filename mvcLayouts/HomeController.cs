3e011895b20010c1e34618e318c754cb75b8d4e9
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-11-15 20:05:08 +0530

    Add files via upload

---
+++ mvcLayouts/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcLayouts.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
