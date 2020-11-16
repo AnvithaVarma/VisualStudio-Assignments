c061b3ba36a1dcfda1c45e49a51ef16eef52b8c0
AnvithaVarma <72150226+AnvithaVarma@users.noreply.github.com>
2020-11-16 08:30:39 +0530

    Add files via upload

---
+++ DbFirstApproach/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBFirstApproach.Models;
+
+namespace DBFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration obj)
+        {
+            if (ModelState.IsValid)
+            {
+                DbApproachEntities db = new DbApproachEntities();
+                db.Registrations.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
